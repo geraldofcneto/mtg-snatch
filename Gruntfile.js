03d8b6f11a5e4559f075c1a3fbb46467cd606e7b
Geraldo Castro <geraldofcneto@gmail.com>
2016-10-01 12:38:35 +0000

    Adding alternative live-reload ports

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,10 +70,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: grunt.option('port') || 9000,
+        port: grunt.option('app_port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
-        livereload: 35729
+        livereload: grunt.option('livereload_port') || 35729
       },
       livereload: {
         options: {
